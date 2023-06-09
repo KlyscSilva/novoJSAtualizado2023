219414d3d49ce7f80872b5395e49723ce1c89aeb
KlyscSilva <smatheussilva@hotmail.com>
2023-06-08 21:49:23 -0300

    up

--- random.js
+++ random.js
@@ -1,4 +1,24 @@
-const input = document.getElementById("numberOne");
-input.value = "random()"
-console.log(input);
+const button = document.getElementById("but")
+const inputResult = document.getElementById("result")
 
+
+function randomNumber() {
+
+  const min = document.getElementById("input-min").value;
+  const max = document.getElementById("input-max").value;
+  
+
+  let inputMin = Math.ceil(min);
+  let inputMax = Math.floor(max);
+
+  const result =
+    Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin;
+
+    inputResult.value = `Número sorteado: ${result}`
+
+  
+}
+
+
+
+button.addEventListener("click",randomNumber)
